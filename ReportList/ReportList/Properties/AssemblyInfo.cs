bab2f09bd96ec65e790513985c0de62c9b110bc2
connewm <32915683+connewm@users.noreply.github.com>
2018-12-11 17:00:34 -0500

    Update AssemblyInfo.cs

--- ReportList/ReportList/Properties/AssemblyInfo.cs
+++ ReportList/ReportList/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProgrammingAssignment4")]
+[assembly: AssemblyTitle("ReportList")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProgrammingAssignment4")]
+[assembly: AssemblyProduct("ReportList")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
